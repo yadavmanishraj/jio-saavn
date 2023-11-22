bc0ff57696e284444bcedfe7c9b2752ffdf1385e
Manish Raj Yadav <yadavmanishrajdev@gmail.com>
2023-11-22 21:10:16 +0545

    Started JioSaavn Client API

---
+++ example/jio_saavn_example.dart
@@ -0,0 +1,5 @@
+void main() {
+  final String $counter = '4';
+  final int counter = int.parse($counter);
+  print(counter);
+}
---
+++ lib/jio_saavn.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/jio_saavn_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
