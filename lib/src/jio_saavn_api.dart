f3fda8c97380def86af8c1c7e73321fceb2ead09
Manish Raj Yadav <yadavmanishrajdev@gmail.com>
2023-11-28 21:13:45 +0545

    Extend JiosaavnApi with Base Client and override send and close

--- lib/src/jio_saavn_api.dart
+++ lib/src/jio_saavn_api.dart
@@ -1,3 +1,16 @@
-class JioSaavnApi {
-  
+import 'package:http/http.dart';
+
+class JioSaavnApi extends BaseClient {
+  final Client _client;
+  JioSaavnApi({Client? client}) : _client = client ?? Client();
+
+  @override
+  Future<void> close() async {
+    _client.close();
+  }
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    return request.send();
+  }
 }
